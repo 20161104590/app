ed97af92754cf1126978bc6e33de9bf4d148b146
20161104590 <a20161104590@king-2.local>
2018-09-26 11:49:20 +0800

    Initial Commit

---
+++ appTests/appTests.swift
@@ -0,0 +1,34 @@
+//
+//  appTests.swift
+//  appTests
+//
+//  Created by 20161104590 on 2018/9/26.
+//  Copyright © 2018年 20161104590. All rights reserved.
+//
+
+import XCTest
+@testable import app
+
+class appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
