a978025efced16e99305facfc7674bbe02071708
20161104590 <a20161104590@King-2.local>
2018-09-28 17:41:39 +0800

    q
    q

--- app/ViewController.swift
+++ app/ViewController.swift
@@ -10,11 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var result_1: UITextField!
+    
+    
+    @IBOutlet weak var result_2: UITextField!
+    
+    @IBOutlet weak var result: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
+    var re = 0
+    
+    
+    @IBAction func n_1(_ sender: Any) {
+    }
+    
+    @IBAction func n_2(_ sender: Any) {
+    }
+    
+    @IBAction func n_3(_ sender: Any) {
+    }
+    
+    @IBAction func n_4(_ sender: Any) {
+    }
+    
+    @IBAction func n_5(_ sender: Any) {
+    }
+    
+    @IBAction func n_6(_ sender: Any) {
+    }
+    
+    @IBAction func n_7(_ sender: Any) {
+    }
+    
+    @IBAction func n_8(_ sender: Any) {
+    }
+    
+    @IBAction func n_9(_ sender: Any) {
+    }
+    
+    @IBAction func n_0(_ sender: Any) {
+    }
+    
+    @IBAction func add(_ sender: Any) {
+    }
+    
+    @IBAction func minus(_ sender: Any) {
+    }
+    
+    
+    
 }
 
