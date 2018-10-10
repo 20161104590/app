fca9426f392f875804f630e5433dad04255059bc
20161104590 <a20161104590@King-2.local>
2018-10-10 11:47:40 +0800

    b
    b

--- app/ViewController.swift
+++ app/ViewController.swift
@@ -12,11 +12,9 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var result_1: UITextField!
     
-    
     @IBOutlet weak var result_2: UITextField!
     
     @IBOutlet weak var result: UILabel!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,37 +22,83 @@ class ViewController: UIViewController {
         
     }
     var re = 0
-    
-    
     @IBAction func n_1(_ sender: Any) {
+        if re == 1{
+            result.text = "1"
+        }else{
+            result.text = result.text! + "1"
+        }
     }
     
     @IBAction func n_2(_ sender: Any) {
+        if re == 1{
+            result.text = "2"
+        }else{
+            result.text = result.text! + "2"
+        }
     }
     
     @IBAction func n_3(_ sender: Any) {
+        if re == 1{
+            result.text = "3"
+        }else{
+            result.text = result.text! + "3"
+        }
     }
     
     @IBAction func n_4(_ sender: Any) {
+        if re == 1{
+            result.text = "4"
+        }else{
+            result.text = result.text! + "4"
+        }
     }
     
     @IBAction func n_5(_ sender: Any) {
+        if re == 1{
+            result.text = "5"
+        }else{
+            result.text = result.text! + "5"
+        }
     }
     
     @IBAction func n_6(_ sender: Any) {
+        if re == 1{
+            result.text = "6"
+        }else{
+            result.text = result.text! + "6"
+        }
     }
     
     @IBAction func n_7(_ sender: Any) {
+        if re == 1{
+            result.text = "7"
+        }else{
+            result.text = result.text! + "7"
+        }
     }
     
     @IBAction func n_8(_ sender: Any) {
+        if re == 1{
+            result.text = "8"
+        }else{
+            result.text = result.text! + "8"
+        }
     }
-    
-    
     @IBAction func n_9(_ sender: Any) {
+        if re == 1{
+            result.text = "9"
+        }else{
+            result.text = result.text! + "9"
+        }
     }
     
     @IBAction func n_0(_ sender: Any) {
+        if re == 1{
+            result.text = "0"
+        }else{
+            result.text = result.text! + "0"
+        }
     }
     
     @IBAction func add(_ sender: Any) {
